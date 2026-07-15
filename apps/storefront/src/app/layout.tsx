f1079fd72f203d8dfc053a6c2df0ea94deb4f8c3
yeasin2002 <yeasin2002.pro@gmail.com>
2026-07-16 02:19:34 +0600

    feat: creatd login page design

--- apps/storefront/src/app/layout.tsx
+++ apps/storefront/src/app/layout.tsx
@@ -34,8 +34,8 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <ThemeProvider 
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          // enableSystem
           disableTransitionOnChange
         >
           {children}  
