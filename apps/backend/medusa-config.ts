40a7df7f007b8a41e000bdab04c09df4d21a3251
yeasin2002 <yeasin2002.pro@gmail.com>
2026-07-18 01:34:33 +0600

    chore: added rsc-labs-medusa-store-analytics-v2 in madusa

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -5,6 +5,7 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    redisUrl: process.env.REDIS_URL,
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
@@ -12,5 +13,11 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET,
       cookieSecret: process.env.COOKIE_SECRET,
     }
-  }
+  },
+  plugins: [
+    {
+      resolve: "@rsc-labs/medusa-store-analytics-v2",
+      options: {},
+    },
+  ],
 })
